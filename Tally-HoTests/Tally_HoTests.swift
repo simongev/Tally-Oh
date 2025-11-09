59e48c93b14a4ae2bdca1354acf2333fef987e63
simongev <gevsimon.1@gmail.com>
2025-11-09 08:29:48 -0500

    Initial Commit

---
+++ Tally-HoTests/Tally_HoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_HoTests.swift
+//  Tally-HoTests
+//
+//  Created by user279199 on 11/9/25.
+//
+
+import Testing
+@testable import Tally_Ho
+
+struct Tally_HoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
